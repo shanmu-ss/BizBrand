56a32df395b0bc51c6697b573be78ec823f231ab
shanmu <76567427+shanmu-ss@users.noreply.github.com>
2025-01-19 02:51:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -110,7 +110,7 @@ export default function App() {
   }, [pathname]);
 
   // Log the port being used
-  const port = process.env.PORT || 8080; // Use Azure's default PORT or fallback // Default to 8080
+  const port = process.env.SERVER_PORT || 8080; // Use Azure's default PORT or fallback // Default to 8080
   console.log(`Running on port: ${port}`);
 
   const getRoutes = (allRoutes) =>
